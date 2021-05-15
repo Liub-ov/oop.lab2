672df34093ee4a33814095c2f9ce5dab5ad18074
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-05-15 15:38:24 +0300

    Add files via upload

---
+++ Cloud.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp3
+{
+    public class Cloud
+    {
+        public int size { get; set; }
+        public string color { get; set; }
+        public string type { get; set; }
+        public int high { get; set; }
+        public Cloud(int size, string color, string type, int high)
+        {
+            this.size = size;
+            this.color = color;
+            this.type = type;
+            this.high = high;
+        }
+        public Cloud Clone()
+        {
+            return new Cloud(size, color, type, high);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cloud = new Cloud(4, "blue", "storm cloud", 500);
+            var result = cloud.Clone();
+            Assert.AreEqual(result.size, cloud.size);
+            Assert.AreEqual(result.color, cloud.color);
+            Assert.AreEqual(result.type, cloud.type);
+            Assert.AreEqual(result.high, cloud.high);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var cloud = new Cloud(15, "white", "regular cloud", 1500);
+            var result = cloud.Clone();
+            Assert.AreEqual(result.size, cloud.size);
+            Assert.AreEqual(result.color, cloud.color);
+            Assert.AreEqual(result.type, cloud.type);
+            Assert.AreEqual(result.high, cloud.high);
+        }
+    }
+}
